669edcdf413e1c378c752f4afbdc0f17240dc1b3
Christian <111076892+i-christian@users.noreply.github.com>
2022-11-29 20:14:44 +0000

    f-strings

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for digit1 in range(0, 10):
     for digit2 in range(digit1 + 1, 10):
         if digit1 == 8 and digit2 == 9:
-            print(f"{digit1:d}{digit2:d}")
+            print(f"{digit1}{digit2}")
         else:
-            print(f"{digit1:d}{digit2:d}", end=", ")
+            print(f"{digit1}{digit2}", end=", ")
