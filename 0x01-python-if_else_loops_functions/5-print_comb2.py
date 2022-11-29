2b44a967621cfed335688b88021b41eafcd2e00c
Christian <111076892+i-christian@users.noreply.github.com>
2022-11-29 19:15:39 +0000

    a function that prints a string in uppercase followed by a new line

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number == 99:
-        print(f"{number:2d}")
+        print(f"{number:d}")
     else:
         print(f"{number:02d}", end=", ")
