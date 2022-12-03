08fe4b596c048434329199f87c708cae19c33133
Christian <111076892+i-christian@users.noreply.github.com>
2022-12-03 17:43:19 +0000

    a function that replaces an element of a list at a specific position (like in C).

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,9 @@
 
 """a function that retrieves an element from a list like in C"""
 
+
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
         return None
     else:
-        return(my_list[idx])
+        return (my_list[idx])
