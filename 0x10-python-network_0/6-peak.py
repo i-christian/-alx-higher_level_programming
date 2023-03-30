6e620e2f94d8d34b6112de23fd6954f67115ec1c
Christian <111076892+i-christian@users.noreply.github.com>
2023-03-31 00:13:52 +0200

    advanced tasks

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers using a binary search algorithm.
+
+    Args:
+        list_of_integers: A list of unsorted integers.
+
+    Returns:
+        A peak in the list of integers. A peak is defined as an element that is greater than or equal to its neighbors.
+    """
+
+    if not list_of_integers:
+        return None
+    elif len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    # Initialize left and right pointers to first and last indices of the list, respectively.
+    left = 0
+    right = len(list_of_integers) - 1
+
+    # Loop while left pointer is less than right pointer.
+    while left < right:
+
+        # Find the middle index between left and right pointers.
+        mid = (left + right) // 2
+
+        # Compare element at mid index with its right neighbor at mid+1.
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            # Peak must be to the right of mid. Update left pointer to mid+1.
+            left = mid + 1
+        else:
+            # Peak must be to the left of mid or at mid itself. Update right pointer to mid.
+            right = mid
+
+    # Return peak element at left pointer.
+    return list_of_integers[left]
