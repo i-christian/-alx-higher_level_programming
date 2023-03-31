66fcddc72312d232abfa467de3ffbf202152cf16
Christian <111076892+i-christian@users.noreply.github.com>
2023-03-31 23:11:59 +0200

    a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,8 @@ from urllib.request import urlopen
 with urlopen("https://alx-intranet.hbtn.io/status") as response:
     body = response.read()
 
-
 if __name__ == "__main__":
-   print( body[:15])
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
