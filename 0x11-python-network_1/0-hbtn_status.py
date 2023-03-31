27b84635ca82328f6eb0c8ffba2e6d113c2b8f6d
Christian <111076892+i-christian@users.noreply.github.com>
2023-03-31 23:16:34 +0200

    add python docs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from urllib.request import urlopen
 """A Python script that fetches https://alx-intranet.hbtn.io/status"""
-
+from urllib.request import urlopen
 
 with urlopen("https://alx-intranet.hbtn.io/status") as response:
     body = response.read()
