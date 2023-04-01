7ad79eb8b246a94cce3921121db9079bbcf81474
Christian <111076892+i-christian@users.noreply.github.com>
2023-04-01 14:27:26 -0700

    a Python script that takes in a URL, sends a request to the URL and displays the body of the response.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """a Python script that takes in a URL, sends a request to the URL and
 displays the body of the response (decoded in utf-8)"""
-from requests.exceptions import HTTPError
 from requests import get
 import sys
 
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    try:
-        with get(url) as response:
+    with get(url) as response:
+        if response.status_code >= 400:
+            print(f"Error code: {response.status_code}")
+        else:
             print(response.text)
-    except HTTPError as err:
-        code = err.response.status_code
-        if code >= 400:
-            print(f"Error code: {err.code}")
