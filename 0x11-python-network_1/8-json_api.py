107317d7d3280941874cbb264669afb7f713473f
Christian <111076892+i-christian@users.noreply.github.com>
2023-04-02 01:35:24 +0200

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -18,8 +18,8 @@ if __name__ == '__main__':
     try:
         json_data = response.json()
         if json_data:
-            print("[{}] {}".format(json_data['id'], json_data['name']))
+            print(f"[{json_data[id]}] {json_data['name']}")
         else:
             print("No result")
     except ValueError:
-        print("Not a valid JSON")
+        print("No result")
