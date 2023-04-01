ccb16963241891e683a0ee2e65d064251fd7ddef
Christian <111076892+i-christian@users.noreply.github.com>
2023-04-02 01:39:10 +0200

    am tired now

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     try:
         json_data = response.json()
         if json_data:
-            print(f"[{json_data[id]}] {json_data['name']}")
+            print(f"[{json_data['id']}] {json_data['name']}")
         else:
             print("No result")
     except ValueError:
