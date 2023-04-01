d9776542ff6334a2c8d3e0cd3c4de3f61adeff07
Christian <111076892+i-christian@users.noreply.github.com>
2023-04-01 23:47:24 +0200

    update files

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,18 +6,20 @@ http://0.0.0.0:5000/search_user with the letter as a parameter.
 import requests
 import sys
 
-if len(sys.argv) > 1:
-    letter = sys.argv[1]
-else:
-    letter = ""
-
-response = requests.post("http://0.0.0.0:5000/search_user", data={'q': letter})
-
-try:
-    json_data = response.json()
-    if json_data:
-        print("[{}] {}".format(json_data['id'], json_data['name']))
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
     else:
-        print("No result")
-except ValueError:
-    print("Not a valid JSON")
+        letter = ""
+
+    response = requests.post(
+        "http://0.0.0.0:5000/search_user", data={'q': letter})
+
+    try:
+        json_data = response.json()
+        if json_data:
+            print("[{}] {}".format(json_data['id'], json_data['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
