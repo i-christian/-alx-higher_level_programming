d848e072f9a8b62ec54bbcac63e9c2b95aafd12e
Christian <111076892+i-christian@users.noreply.github.com>
2023-12-01 12:15:55 +0200

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8)"""
+from urllib.error import HTTPError
+import sys
+from urllib.request import urlopen
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    try:
+        with urlopen(url) as response:
+            content = response.read().decode('utf-8')
+            print(content)
+    except HTTPError as err:
+        print(f"Error code: {err.code}")
