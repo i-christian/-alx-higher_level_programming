fdbb50df6b5205ed49e8c195afd86607c12c8658
Christian <111076892+i-christian@users.noreply.github.com>
2023-12-01 12:18:30 +0200

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    response = requests.post(
+        "http://0.0.0.0:5000/search_user", data={'q': letter})
+
+    try:
+        json_data = response.json()
+        if json_data:
+            print(f"[{json_data['id']}] {json_data['name']}")
+        else:
+            print("No result")
+    except ValueError:
+        print("No result")
